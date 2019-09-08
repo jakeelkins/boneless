bc9e8961262ae9076844b59d1c46f2cfdf51a5fe
Jake Elkins <45544450+jakeelkins@users.noreply.github.com>
2019-09-08 15:47:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='spongebob',
+setup(name='boneless',
       version='0.0.1',
       description='can i get a boneless pizza',
       long_description='lets just say...if this works youll never use skype the same again',
